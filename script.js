9d2569b3327eebba6a50f38f15ee5032a7367f94
Aqsa Saleheene <angieclrk21@gmail.com>
2022-03-03 20:01:11 -0500

    HW

--- script.js
+++ script.js
@@ -19,17 +19,40 @@ window.onload =  function(event) {
 
   // Do all of your work inside the window.onload function (in other words, here!)
 
-  // Part 1
+  // Part 1 
+  document.querySelector('h1').innerText = 'The Dominator';
 
   // Part 2
+  document.body.style.backgroundColor = 'green'
 
   // Part 3
+  document.querySelectorAll('li')[5].remove()
 
   // Part 4
+  document
+  .querySelectorAll('.special-title')
+  .forEach((specialTitle) => (specialTitle.style.fontSize = '2rem'))
 
   // Part 5
+  document.querySelectorAll('li')[8].remove()
+
 
   // Part 6
+  let newLocation = document.createElement('li')
+  newLocation.textContent = 'Munich'
+  document.querySelector('#past-races').appendChild(newLocation);
 
   // Part 7
+let div = document.createElement('div');
+div.className = 'blog-post purple';
+let main = document.querySelector('.main');
+main.insertBefore(div, main.children[4]);
+
+let title = document.createElement('h1');
+title.innerText = 'Munich';
+div.appendChild(title)
+
+let text=document.createElement('p');
+text.innerText = "I RACED AROUND OKTOBERFEST!";
+div.appendChild(text)
 }
